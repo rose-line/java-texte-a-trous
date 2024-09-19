93ab0a93c64eda396b72fd25bb877c50d1729d7e
rose-line <1886792+rose-line@users.noreply.github.com>
2024-09-19 14:02:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# java-texte-a-trous
---
+++ src/main/java/fr/dampierre/App.java
@@ -0,0 +1,21 @@
+package fr.dampierre;
+
+import java.util.Scanner;
+
+public class App {
+
+  public static void main(String[] args) {
+    Scanner clavier = new Scanner(System.in);
+
+    System.out.print("Entrez le premier nom commun : ");
+    String nomCommun1 = clavier.next();
+    System.out.print("Entrez le nombre : ");
+    int nombre = clavier.nextInt();
+    System.out.print("Entrez le deuxième nom commun : ");
+    String nomCommun2 = clavier.next();
+    System.out.print("Entrez l'adjectif : ");
+    String adjectif = clavier.next();
+    System.out.println("Il était une fois un " + nomCommun1 + " qui avait " + nombre + " "
+        + nomCommun2 + ". Cela le rendait très " + adjectif + ".");
+  }
+}
